e50286d44c1daa48496eae3f4ad1de2153dcd964
grahamsnz <graham@cloudpbx.ca>
2016-11-29 22:26:21 -0800

    Start of French translation of Kazoo-UI

---
+++ config/lang/fr.js
@@ -0,0 +1,71 @@
+window.translate['config'] = {
+	voip_label: "PBX hébergé",
+	voip_desc: "Gestion des boites vocales, routage d'appels...",
+	cluster_label: "Gestion de grappe",
+	cluster_desc: "Gestion de serveurs et d'infrastructure...",
+	userportal_label: "Outil d’utilisateurs",
+	userportal_desc: "Donner accès à utilisateur à sa boite vocale, son historique d’appel, et son renvoi d’appels...",
+	accounts_label: "Comptes",
+	accounts_desc: "Gestion de sous-comptes...",
+	developer_label: "Développeur",
+	developer_desc: "Outil API pour développeur...",
+	pbxs_label: "Connecteur de PBX",
+	pbxs_desc: "Gestion de connexions SIP pour vos PBXs",
+	numbers_label: "Numéros",
+	numbers_desc: "Gestion de numéros...",
+	browserphone_label: "Téléphone web",
+	browserphone_desc: "Votre téléphone SIP web",
+	there_were_errors: "Il y avait des erreurs sur le formulaire, s'il vous plaît corriger!", //all errors on the form
+	OK: "D'accord",		//button OK confirm dialog
+	CANCEL: "Annuler",	//button Cancel confirm dialog
+	CLOSE: "Fermé",		//button Close alert dialog
+ 	hide: "Cacher",
+ 	show: "Exposer",
+ 	hide_all: "Cacher tout",
+ 	show_all: "Exposer tout",
+	please_confirm_title: "S'il vous plaît confirmer",	//confirm title dialog
+	WARNING: "avertissement",
+			
+	/*-------------category actions--------*/
+	basic_cat: "Essentiel",
+	advanced_cat: "Supplémentaire",
+	call_forwarding_cat: "Renvoi d’appels",
+	caller_id_cat: "Afficher l'appelant",
+	call_recording_cat: "Enregistrement des appels",
+	call_center_cat: "Centre d'appel",
+	hotdesking_cat: "Bureau partagé ",
+	time_of_day_cat: "Heures ouvrables",
+	/*-------------------------------------*/
+	advanced_menu_cat: "Supplémentaire",
+	
+	/*-------------jquery.dataTable--------*/
+	sProcessing: "En traitement...",
+	sLengthMenu: "Montrer les options _MENU_",
+	sZeroRecords: "Aucune donnée correspondante",
+	sEmptyTable: "Tableur ne contient aucune donnée",
+	sLoadingRecords: "Chargement...",
+	sInfo: "Affichage de _START_ à _END_ des _TOTAL_ entrées totales.",
+	sInfoEmpty: "Affichage de 0 à 0 des 0 entrées totales",
+	sInfoFiltered: "(filtré des _MAX_ entrées totales)",
+	sInfoPostFix: "",
+	sSearch: "Chercher:",
+	sUrl: "",
+	sFirst: "Première",
+	sPrevious: "Précédent",
+	sNext: "Prochaine",
+	sLast: "Dernier",
+	/*-------------------------------------*/
+	
+	alert_logout: "Votre session va expirer pour cause d'inactivité. Veuillez déplacer votre curseur si vous souhaitez rester connecté.",
+	charges_summary_title: "Récapitulatif des frais",
+	content_charges: "Voici les détails des frais mensuels pour les services liés à votre compte:",
+	there_is_no: "Il n'y a pas de frais pour l'action: ",
+	you_will_pay: "Vous paierez un fraus unuique de ",
+	one_time: "$ pour ",
+	service: "Service",
+	rate: "Taux",
+	quantity: "Quantité",
+	discount: "Rabais",
+	monthly_charges: "Frais mensuels",
+	press_OK_or_Cancel: "Appuyez sur D'accord pour continuer ou sur Annuler pour interrompre le processus."
+};
